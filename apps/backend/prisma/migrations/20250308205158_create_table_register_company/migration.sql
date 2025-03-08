f81a93c6bbcc3ec9bc35e0992f2e29f795d556c5
Wesley Bruno <engwesleybruno@gmail.com>
2025-03-08 17:53:23 -0300

    implementa fluxo de tabela de registro e autenticacao no banco de dados

---
+++ apps/backend/prisma/migrations/20250308205158_create_table_register_company/migration.sql
@@ -0,0 +1,64 @@
+/*
+  Warnings:
+
+  - The primary key for the `permissions` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `code` on the `permissions` table. All the data in the column will be lost.
+  - You are about to drop the column `created_at` on the `permissions` table. All the data in the column will be lost.
+  - You are about to drop the column `description` on the `permissions` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `permissions` table. All the data in the column will be lost.
+  - The `id` column on the `permissions` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - A unique constraint covering the columns `[cnpj]` on the table `companies` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[phone]` on the table `companies` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[email]` on the table `companies` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[name]` on the table `permissions` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `name` to the `permissions` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "companies" ADD COLUMN     "city" TEXT,
+ADD COLUMN     "cnpj" TEXT,
+ADD COLUMN     "complement" TEXT,
+ADD COLUMN     "email" TEXT,
+ADD COLUMN     "neighborhood" TEXT,
+ADD COLUMN     "number" TEXT,
+ADD COLUMN     "phone" TEXT,
+ADD COLUMN     "state" TEXT,
+ADD COLUMN     "street" TEXT,
+ADD COLUMN     "zipCode" TEXT;
+
+-- AlterTable
+ALTER TABLE "permissions" DROP CONSTRAINT "permissions_pkey",
+DROP COLUMN "code",
+DROP COLUMN "created_at",
+DROP COLUMN "description",
+DROP COLUMN "updated_at",
+ADD COLUMN     "name" TEXT NOT NULL,
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+ADD CONSTRAINT "permissions_pkey" PRIMARY KEY ("id");
+
+-- CreateTable
+CREATE TABLE "user_permissions" (
+    "userId" TEXT NOT NULL,
+    "permissionId" INTEGER NOT NULL,
+
+    CONSTRAINT "user_permissions_pkey" PRIMARY KEY ("userId","permissionId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "companies_cnpj_key" ON "companies"("cnpj");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "companies_phone_key" ON "companies"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "companies_email_key" ON "companies"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "permissions_name_key" ON "permissions"("name");
+
+-- AddForeignKey
+ALTER TABLE "user_permissions" ADD CONSTRAINT "user_permissions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_permissions" ADD CONSTRAINT "user_permissions_permissionId_fkey" FOREIGN KEY ("permissionId") REFERENCES "permissions"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
